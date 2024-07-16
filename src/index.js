56eb9f7923d8e6ad0d78c7deae956f78417211ae
vpalmerio <vpalmerio@protonmail.com>
2024-07-16 14:58:57 -0500

    Upgraded all packages and made subsequent fixes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Router><App /></Router>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

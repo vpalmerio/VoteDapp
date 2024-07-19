2e205be8dc2e4cf9446cb23bc9e0e3bf8e84d1a9
vpalmerio <vpalmerio@protonmail.com>
2024-07-19 18:54:28 -0500

    Created PollName component, improving code efficiency

---
+++ src/components/PollName.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import AppPage from './AppPage'
+import Button from './Button'
+import BackButton from './BackButton'
+
+export default function PollName({ changePollName , changePage, talkToContractInteraction}) {
+
+    const [pollNameInput, changePollNameInput] = React.useState("")
+    const [nameTaken, changeNameTaken] = React.useState(false)
+
+    const nextPage = () => {
+        changePollName(pollNameInput)
+        changePage("Description")
+    }
+
+    return (
+        <AppPage
+        title="Create a poll!"
+        description="Create your own customizable poll here!"
+        >
+            <p>&nbsp;</p>
+            <div className="text-center style={{ maxWidth: '400px' }}">
+                <p>&nbsp;</p>
+                <h1> Your Poll's Name </h1>
+                <p> What's the main idea? Short words are better. </p>
+
+                {nameTaken
+                    && <p className="text-left text-danger"> This poll name has already been taken. </p>
+                }
+                <div className="mr-sm-2">
+                
+                    {nameTaken
+                    ?<input
+                        type="text"
+                        onChange={(input) => {
+                            talkToContractInteraction("Storage", false, "pollNameExists", [input.target.value], "Loading...").then((result) => {
+                                if (result !== nameTaken) {
+                                    changeNameTaken(result)
+                                }
+                            })
+                            changePollNameInput(input.target.value)
+                        }}
+                        className="form-control"
+                        value={pollNameInput}
+                        style={{ borderColor: 'red'}}
+                        required />
+                    :<input
+                        type="text"
+                        onChange={(input) => {
+                            talkToContractInteraction("Storage", false, "pollNameExists", [input.target.value], "Loading...").then((result) => {
+                                if (result !== nameTaken) {
+                                    changeNameTaken(result)
+                                }
+                            })
+                            changePollNameInput(input.target.value)
+                        }}
+                        className="form-control"
+                        value={pollNameInput}
+                        required />
+                    }
+                </div>
+
+                <p>&nbsp;</p>
+
+                {pollNameInput === ""
+                    ||<div>
+                    {nameTaken
+                        || <Button onClick = {() => nextPage()} text="Next"></Button>
+                    }
+                    </div>
+                }
+                
+                <p>&nbsp;</p>
+
+                <BackButton/>
+
+                <p>&nbsp;</p>
+            </div>
+        </AppPage>
+    )
+}

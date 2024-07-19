1fe358148e54d422672947b29232e980fcee527f
vpalmerio <vpalmerio@protonmail.com>
2024-07-19 15:04:19 -0500

    Use constants for page links for more modularity

---
+++ src/components/Constants.js
@@ -0,0 +1,20 @@
+
+// Links to each page
+export const APP_LINK = "/app"
+
+export const OWNED_POLLS_LINK = APP_LINK + "/owned"
+
+export const PARTICIPATED_POLLS_LINK = APP_LINK + "/participated"
+
+export const EXPLORE_LINK = APP_LINK + "/explore"
+
+export const MANAGE_VDA_LINK = APP_LINK + "/manage-vda"
+
+export const POLL_LINK = APP_LINK + "/polls"
+
+export const CHOOSE_POLL_TYPE_LINK = APP_LINK + "/choose-poll-type"
+
+export const CREATE_POLL_TYPE_LINK = APP_LINK + "/create-poll"
+export const CREATE_POLL_TYPE_LINK_REGULAR = CREATE_POLL_TYPE_LINK + "/Regular"
+export const CREATE_POLL_TYPE_LINK_QUADRATIC = CREATE_POLL_TYPE_LINK + "/Quadratic"
+export const CREATE_POLL_TYPE_LINK_RANKED = CREATE_POLL_TYPE_LINK + "/Ranked"

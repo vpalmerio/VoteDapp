7329408abbd9ffddf58285cc0ace0b45bb159930
vpalmerio <vpalmerio@protonmail.com>
2024-07-24 01:47:06 -0500

    Modified Home.js to use new components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,21 +4,20 @@ import createpoll1 from '../media/createpoll1.gif'
 import createpoll2 from '../media/createpoll2.gif'
 import createpoll3 from '../media/createpoll3.gif'
 
+import Jumbotron from '../components/Jumbotron'
+import Footer from '../components/Footer'
+
 export default function Home() {
 
 	return(
 
-	<div className="container-fluid">
-    
+	  <div className="container-fluid">
       <p></p>
-    
-      <section className="jumbotron card bg-primary">
-        <div className="container text-center py-2">
-          <h1 className="display-4">VoteDapp (Goerli Testnet)</h1>
-          <p className="lead text-white">Decentralized, free, open. VoteDapp is an easy to use poll interface on the ethereum network for anyone, anywhere.</p>
-        </div>
-      </section>
-      	<div className="text-center py-4">
+      <Jumbotron
+        bigText='VoteDapp (Goerli Testnet)'
+        smallText='Decentralized, free, open. VoteDapp is an easy to use poll interface on the ethereum network for anyone, anywhere.'
+      ></Jumbotron>
+      <div className="text-center py-4">
     		<a className="btn btn-primary" href="/app">Head to the App</a>
     	</div>
 
@@ -106,24 +105,7 @@ export default function Home() {
         </div>
       </section>
     
-      <footer className="py-3">
-        <div className="container">
-          <div className="d-flex justify-content-between align-items-center flex-column flex-md-row border-bottom pb-2">
-            <h5 className="ml-0 ml-md-3 mb-0"><a href="/app">App</a></h5>
-            <ul className="nav justify-content-center">
-              <li className="nav-item"><a className="nav-link" href="https://github.com/rokkinrob/VoteDapp">Github</a></li>
-              <li className="nav-item"><a className="nav-link" href="https://discord.gg/uEeFU7n">Discord</a></li>
-            </ul>
-          </div>
-          <div className="d-flex justify-content-center align-items-center flex-column flex-md-row mx-3 mt-3">
-            <div className="d-flex mt-3 mt-md-0">
-              <p className="mb-0 small text-muted">VoteDapp</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer/>
     </div>
-
 	)
-
 }

3aff50121f4bb6bf432f7102166e4dfa09c9e49b
Vincent Palmerio <62448074+vpalmerio@users.noreply.github.com>
2024-07-24 01:47:34 -0500

    Replaced Fakemain with PlaceholderMain, utilizing new components

--- src/pages/PlaceholderMain.js
+++ src/pages/PlaceholderMain.js
@@ -1,47 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
-/*
-*
-* TODO:
-* Convert Fakename routes to use element
-* Reimplement withRouter in App.js 
-* Use copilot to fix error when copilot is fixed?
-*
-*/
-
-import {Helmet} from "react-helmet";
+import {Helmet} from "react-helmet"
 
 import loader from '../media/loader.png'
+import AppPage from '../components/AppPage'
 
-export default function Fakemain(props) { 
+export default function PlaceholderMain(props) { 
 
       return (
         <div>
-          <div className="container-fluid mt-5">
-            <div className="row justify-content-center">
-              <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '800px' }}>
-                <div className="content mr-auto ml-auto">
-
-                {props.loadingDescription == "Failed to connect to contracts. Please make sure you are on the Goerli testnet. Get Goerli testnet eth at https://goerli-faucet.slock.it/."
-                  ? <p className="text-danger text-center">{"Failed to connect to contracts. Please make sure you are on the Goerli testnet. Get Goerli testnet eth at https://goerli-faucet.slock.it/."}</p>
-                  : <div className="text-center">
-                      <p>Loading...</p>
-                      <img 
-                        src={loader} 
-                        alt="Ballot Box" 
-                        width="64"
-                        height="64"
-                      />
-                    </div>
-
-                }
-                
-
+          <AppPage>
+            {props.failedToLoad
+              ? <p className="text-danger text-center">{props.loadingDescription}</p>
+              : <div className="text-center">
+                  <p>Loading...</p>
+                  <img 
+                    src={loader} 
+                    alt="Ballot Box" 
+                    width="64"
+                    height="64"
+                  />
                 </div>
-              </main>
-            </div>
-          </div>
+            }
+          </AppPage>
           <div>
             <main>
             <Routes>
@@ -101,7 +83,9 @@ export default function Fakemain(props) {
                 </Helmet>
               } />
 
-              <Route element = { <Wrongpage/> } />
+              <Route path="*" element = {
+                <Wrongpage changeLoadingBlockchain={props.changeLoadingBlockchain}/>
+              } />
               </Routes>
             </main>
           </div>
@@ -110,21 +94,11 @@ export default function Fakemain(props) {
       );
 }
 
-const Wrongpage = () => {
+const Wrongpage = ({changeLoadingBlockchain}) => {
 
+    changeLoadingBlockchain(false)
     return (
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
-            <div className="content mr-auto ml-auto">
-
-            <p className="text-center">Unfortunately, this page does not exist.</p>
-            
-
-
-            </div>
-          </main>
-        </div>
+      <div>
       </div>
     )
 }

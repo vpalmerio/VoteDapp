fcd80cce2ba783db4d9063fb2e348035251557b4
Vincent Palmerio <62448074+vpalmerio@users.noreply.github.com>
2024-07-24 02:34:59 -0500

    Made Button component have specific width

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,26 +2,21 @@ import React from 'react';
 
 import { useNavigate } from "react-router-dom";
 
-export default function Button(props) {
+export default function Button({ path, text, onClick, buttonType = '' }) {
 
     const handleClick = () => {
-        if (props.path !== undefined) {
-            navigate(props.path);
+        if (path !== undefined) {
+            navigate(path);
         }
-        if (props.onClick) {
-            props.onClick();
+        if (onClick) {
+            onClick();
         }
-    }
-
-    let buttonType = ''
-    if (props.buttonType !== undefined) {
-        buttonType = props.type
     }
 
     const navigate = useNavigate()
     return (
         <div className='center-content'>
-            <button className="btn btn-primary btn-block" type={buttonType} onClick = {() => handleClick()}>{props.text}</button>
+            <button className="btn btn-primary btn-block" style={{width: '160px'}} type={buttonType} onClick = {() => handleClick()}>{text}</button>
         </div>
     )
 }

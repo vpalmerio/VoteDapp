bc85200dbcec8b622e8bc18fa2e5d98209a3580f
Vincent Palmerio <62448074+vpalmerio@users.noreply.github.com>
2024-07-25 17:17:30 -0500

    Deployment script writes to file in frontend, saving (in total) hours of setup time

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+
+const { network } = require('hardhat');
+
 const VDTSaleModule = require("../ignition/modules/VoteDappTokenSale");
 const VDQuadraticModule = require("../ignition/modules/VoteDappQuadratic");
 const VDRegularModule = require("../ignition/modules/VoteDappRegular");
@@ -40,11 +44,64 @@ async function main() {
 
   VDStorage.addPollType([VDQAddress, VDRAddress, VDRankedAddress]);
 
+  const chainID = parseInt(await network.provider.send('eth_chainId'));
+  console.log(chainID);
+
+  let contract_data = {
+    networkID: chainID,
+    VoteDappToken: {
+      address: tokenAddress,
+      abi: await getContractABI("VoteDappToken"),
+    },
+    VoteDappStorage: {
+      address: storageAddress,
+      abi: await getContractABI("VoteDappStorage"),
+    },
+    VoteDappQuadratic: {
+      address: VDQAddress,
+      abi: await getContractABI("VoteDappQuadratic"),
+    },
+    VoteDappRegular: {
+      address: VDRAddress,
+      abi: await getContractABI("VoteDappRegular"),
+    },
+    VoteDappRanked: {
+      address: VDRankedAddress,
+      abi: await getContractABI("VoteDappRanked"),
+    },
+  };
+
+  let jsonData = { networkID: {} };
+  fs.readFile('contract_data.json', 'utf8', (err, data) => {
+    if (!err) {
+      jsonData = JSON.parse(data);
+    }
+    //if there is an error, the file probbly doesn't exist
+  });
+
+  
+  jsonData.networkID[chainID] = contract_data;
+  
+  jsonData = JSON.stringify(jsonData);
+
+  fs.writeFile('../src/contract_data.json', jsonData, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('JSON data has been written to /src/contract_data.json');
+    }
+  });
+
   console.log(`VDTSale deployed to: ${tokenAddress}`);
   console.log(`VDStorage deployed to: ${storageAddress}`);
   console.log(`VDQuadratic deployed to: ${VDQAddress}`);
   console.log(`VDRegular deployed to: ${VDRAddress}`);
   console.log(`VDRanked deployed to: ${VDRankedAddress}`);
+}
+
+const getContractABI = async (contractName) => {
+  const artifact = await hre.artifacts.readArtifact(contractName);
+  return artifact.abi;
 }
 
 main().catch(console.error);

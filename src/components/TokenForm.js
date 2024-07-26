e4a9f74ac0165912ff6268aa464777e87346a587
Vincent Palmerio <62448074+vpalmerio@users.noreply.github.com>
2024-07-26 17:41:21 -0500

    Created TokenForm.js to improve code efficiency

---
+++ src/components/TokenForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import Button from '../components/Button'
+
+export default function TokenForm({ tokenFunction, action, placeholder, tokenPrice, buttonText  }) {
+
+    const [amountOfTokensInput, changeAOTI] = React.useState(0)
+
+    //convert final price to Gwei
+    let tempFinalPrice = ((tokenPrice * amountOfTokensInput) / 1000000000)
+
+
+    return (
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            
+            tokenFunction(amountOfTokensInput)
+          }}>
+            <div className='text-center mt-5'>
+              <p>Total Value of VDA you want to {action} in Gwei: {tempFinalPrice}</p>
+            </div>
+            <div className="form-group mr-sm-2">
+              <input
+              id={action}
+              type="number"
+              onChange={(input) => { changeAOTI(input.target.value) }}
+              className="form-control"
+              placeholder={placeholder}
+              required />
+            </div>
+            <div className='mt-4'>
+              <Button buttonType="submit" text={buttonText}></Button>
+            </div>
+        </form>
+    )
+}

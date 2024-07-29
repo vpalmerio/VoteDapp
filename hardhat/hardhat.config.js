89a0e5e101297ac4d83c66e67d956c2e3378d65a
Vincent Palmerio <62448074+vpalmerio@users.noreply.github.com>
2024-07-29 17:50:05 -0500

    Add Sepolia network to hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -7,4 +7,10 @@ const { SEPOLIA_PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.26",
+  networks: {
+    sepolia: {
+      url: `https://sepolia-rpc.scroll.io/` || "",
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };

4f838f65a438e2b8418652841f56157b4fb77f0f
Vincent Palmerio <62448074+vpalmerio@users.noreply.github.com>
2024-07-29 16:02:51 -0500

    Added dotenv setup to hardhat

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const dotenv = require("dotenv");
+dotenv.config();
+const { SEPOLIA_PRIVATE_KEY } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.26",

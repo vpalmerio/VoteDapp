3106ad3b4a59379a7f91d180c61bb41483f6f05f
vpalmerio <vpalmerio@protonmail.com>
2024-07-30 15:27:29 -0500

    Add mainnet network to hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,6 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 const dotenv = require("dotenv");
 dotenv.config();
 const { SEPOLIA_PRIVATE_KEY } = process.env;
+const { INFURA_API_KEY } = process.env;
+const { MAINNET_PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -11,6 +13,10 @@ module.exports = {
     sepolia: {
       url: `https://sepolia-rpc.scroll.io/` || "",
       accounts: [SEPOLIA_PRIVATE_KEY]
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}` || "",
+      accounts: [MAINNET_PRIVATE_KEY]
     }
   }
 };

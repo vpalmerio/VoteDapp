30c1a4c525cde489fb74e313419256e1c7ad9d90
vpalmerio <vpalmerio@protonmail.com>
2024-07-30 20:40:31 -0500

    Fix small UI bug when displaying votes

--- src/components/DisplayVotes.js
+++ src/components/DisplayVotes.js
@@ -2,13 +2,13 @@
  * This component is used in GetPollInfo.js to display the previous votes of the user and the total votes for each option.
  */
 
-export default function DisplayVotes({ votes, options }) {
+export default function DisplayVotes({ votes, options, title }) {
     return (
         <div className='center-content' >
                     
             <div className="card mb-4 bg-dark col-6 border border-white" style={{ maxWidth: '400px' }} >
                 <div className="card-header">
-                <p className='text-white'> Your vote(s): </p>
+                <p className='text-white'>{title}</p>
                 </div>
 
             {votes.map((previousOptionVotes, key) => {
